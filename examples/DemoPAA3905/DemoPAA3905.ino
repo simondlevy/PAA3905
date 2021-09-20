5237f6f066494177f26ff965707170f53dc20734
simondlevy <simon.d.levy@gmail.com>
2021-09-20 19:30:54 -0400

    Start rebuilding

--- examples/DemoPAA3905/DemoPAA3905.ino
+++ examples/DemoPAA3905/DemoPAA3905.ino
@@ -1,202 +1,209 @@
-/*
-   Demo of the PAA3905 optical flow sensor
+/* 08/25/2021 Copyright Tlera Corporation
+ *  
+ *  Created by Kris Winer
+ *  
+ This sketch configures and reads data from the PAA3905 optical flow sensor. 
+ The sensor uses standard SPI for communications at a maximum serial port speed of 2 MHz. The sensor data ready
+ is signaled by an active LOW interrupt.
+ 
+ This sensor offers two sensitivities: standard detection and enhanced detection for rough terrain 
+ at > 15 cm height. The sensor can automatically switch between bright (>60 lux), low light (>30 lux), 
+ and super low light (> 5 lux) conditions. Bright and low light modes work at 126 frames per second. The super 
+ low light mode is limited to 50 frames per second. 
+ 
+ The sensor uses typically 3.5 mA in operation and has a 12 uA shutdown mode The sensor can operate 
+ in navigate mode producing delta X and Y values which are proportional to lateral velocity. 
+ The limiting speed is determined by the maximum 7.2 rads/sec flow rate and by distance to the measurement 
+ surface; 80 mm is the minimum measurement distance. So at 80 mm the maxium speed is 0.576 m/s (1.25 mph), 
+ at 2 meter distance (~drone height) the maximum speed is 14.4 m/s (32 mph), etc. 
+ 
+ The sensor can also operate in raw data (frame grab) mode producing 35 x 35 pixel images from the 
+ sensor at a frame rate of ~15 Hz. This makes the PAA3905 an inexpensive, low-resolution, infrared-sensitive 
+ video camera.
+ 
+ The sketch uses default SPI pins on the Ladybug development board.
 
-   This sketch configures and reads data from the PAA3905 optical flow sensor. 
-   The sensor uses standard SPI for communications at a maximum serial port
-   speed of 2 MHz. The sensor data ready is signaled by an active LOW
-   interrupt.
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
-   This sensor offers two sensitivities: standard detection and enhanced
-   detection for rough terrain at > 15 cm height. The sensor can automatically
-   switch between bright (>60 lux), low light (>30 lux), and super low light (> 5
-   lux) conditions. Bright and low light modes work at 126 frames per second. The
-   super low light mode is limited to 50 frames per second. 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
 
-   The sensor uses typically 3.5 mA in operation and has a 12 uA shutdown mode
-   The sensor can operate in navigate mode producing delta X and Y values which
-   are proportional to lateral velocity.  The limiting speed is determined by the
-   maximum 7.2 rads/sec flow rate and by distance to the measurement surface; 80
-   mm is the minimum measurement distance. So at 80 mm the maxium speed is 0.576
-   m/s (1.25 mph), at 2 meter distance (~drone height) the maximum speed is 14.4
-   m/s (32 mph), etc. 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
 
-   The sensor can also operate in raw data (frame grab) mode producing 35 x 35
-   pixel images from the sensor at a frame rate of ~15 Hz. This makes the PAA3905
-   an inexpensive, low-resolution, infrared-sensitive video camera.
-
-   Copyright (c) 2021 Tlera Corporiation and Simon D. Levy
-
-   MIT License
-
- */
+ Library may be used freely and without limit with proper attribution including this entire preamble.
+*/
 #include <SPI.h>
 #include "PAA3905.h"
-#include "Debugger.hpp"
 
 // Pin definitions
-static const uint8_t CS_PIN  = 10;  // default chip select for SPI
-static const uint8_t MOT_PIN =  8;  // use as data ready interrupt
+#define CSPIN  10  // default chip select for SPI
+#define MOSI   11  // SPI MOSI pin on Ladybug required for frame capture
+#define MOT     8  // use as data ready interrupt
 
-// Sensor configuration
-static PAA3905::detection_mode_t DETECTION_MODE = PAA3905::DETECTION_STANDARD;
-static PAA3905::auto_mode_t      AUTO_MODE      = PAA3905::AUTO_MODE_01; 
-static PAA3905::orientation_t    ORIENTATION    = PAA3905::ORIENTATION_NORMAL;
-static uint8_t                   RESOLUTION     = 0x2A;
+// PAA3905 configuration
+uint8_t mode = standardDetectionMode; // mode choices are standardDetectionMode (default) or enhancedDetectionMode
+uint8_t autoMode = autoMode01;        // choices are autoMode01 and autoMode012 (includes superLowLight mode)
+uint8_t pixelRes = 0x2A;  // choices are from 0x00 to 0xFF
+float resolution;         // calculated (approximate) resolution (counts per delta) per meter of height
+uint8_t orientation, orient = 0x00; // for X invert 0x80, for Y invert 0x40, for X and Y swap, 0x20, for all three 0XE0 (bits 5 - 7 only)
+int16_t deltaX = 0, deltaY = 0;
+uint32_t Shutter = 0;
+volatile bool motionDetect = false;
+uint8_t statusCheck = 0;
+uint8_t frameArray[1225], dataArray[14], SQUAL, RawDataSum = 0, RawDataMin = 0, RawDataMax = 0;
+uint8_t iterations = 0;
+uint32_t frameTime = 0;
 
-static PAA3905 sensor(CS_PIN);
+PAA3905 opticalFlow(CSPIN); // Instantiate PAA3905
 
-static volatile bool gotMotionInterrupt;
-static void interruptHandler()
-{
-    gotMotionInterrupt = true;
+void setup() {
+  Serial.begin(115200);
+  delay(4000);
+  Serial.println("Serial enabled!");
+
+  pinMode(MOT, INPUT); // data ready interrupt
+
+  // Configure SPI Flash chip select
+  pinMode(CSPIN, OUTPUT);
+  digitalWrite(CSPIN, HIGH);
+
+  SPI.begin(); // initiate SPI 
+  delay(1000);
+
+  opticalFlow.begin();  // Prepare SPI port 
+  
+  // Check device ID as a test of SPI communications
+  if (!opticalFlow.checkID()) {
+  Serial.println("Initialization of the opticalFlow sensor failed");
+  while(1) { }
+  }
+
+  opticalFlow.reset(); // Reset PAA3905 to return all registers to default before configuring
+  
+  opticalFlow.setMode(mode, autoMode);         // set modes
+  
+  opticalFlow.setResolution(pixelRes);         // set resolution fraction of default 0x2A
+  resolution = (opticalFlow.getResolution() + 1) * 200.0f/8600.0f; // == 1 if pixelRes == 0x2A
+  Serial.print("Resolution is: "); Serial.print(resolution * 11.914f, 1); Serial.println(" CPI per meter height"); Serial.println(" ");
+  
+  opticalFlow.setOrientation(orient);
+  orientation = opticalFlow.getOrientation();
+  if(orientation & 0x80) Serial.println("X direction inverted!"); Serial.println(" ");
+  if(orientation & 0x40) Serial.println("Y direction inverted!"); Serial.println(" ");
+  if(orientation & 0x20) Serial.println("X and Y swapped!"); Serial.println(" ");
+ 
+  attachInterrupt(MOT, myIntHandler, FALLING); // data ready interrupt active LOW 
+  
+  statusCheck = opticalFlow.status();          // clear interrupt before entering main loop
+
+//  opticalFlow.shutdown();                    // enter lowest power mode until ready to use
+  /* end of setup */
 }
-
-void setup() 
-{
-    Serial.begin(115200);
-    delay(4000);
-
-    pinMode(MOT_PIN, INPUT); // data ready interrupt
-
-    // Configure SPI Flash chip select
-    pinMode(CS_PIN, OUTPUT);
-    digitalWrite(CS_PIN, HIGH);
-
-    SPI.begin(); // initiate SPI 
-    delay(1000);
-
-    sensor.begin();  // Prepare SPI port 
-
-    // Check device ID as a test of SPI communications
-    if (!sensor.checkID()) {
-        Debugger::reportForever("Initialization of the sensor sensor failed");
-    }
-
-    // Reset PAA3905 to return all registers to default before configuring
-    sensor.reset(); 
-
-    sensor.setMode(DETECTION_MODE, AUTO_MODE); 
-
-    sensor.setResolution(RESOLUTION);         
-    Debugger::printf("Resolution is: %f CPI per meter height\n\n",
-            sensor.getResolution());
-
-    sensor.setOrientation(ORIENTATION);
-    uint8_t orientation = sensor.getOrientation();
-    if (orientation & PAA3905::ORIENTATION_XINVERT) {
-        Debugger::printf("X direction inverted!\n");
-    }
-    if (orientation & PAA3905::ORIENTATION_YINVERT) {
-        Debugger::printf("Y direction inverted!\n");
-    }
-    if (orientation & PAA3905::ORIENTATION_SWAP) {
-        Debugger::printf("X and Y swapped!\n");
-    }
-
-    attachInterrupt(MOT_PIN, interruptHandler, FALLING); // data ready interrupt active LOW 
-
-    sensor.status();          // clear interrupt before entering main loop
-
-    //  sensor.shutdown();                    // enter lowest power mode until ready to use
-
-} // setup
 
 void loop() {
 
-    static uint8_t frameArray[1225];
-    static uint8_t iterations;
+  iterations++;
+  
+  // Navigation
+  if(motionDetect){
+     motionDetect = false;
+   
+//   statusCheck = opticalFlow.status(); // clear interrupt
+//   if(statusCheck & 0x01) Serial.println("Challenging surface detected!");
+//   if(statusCheck & 0x80) { //Check that motion data is available
+//   opticalFlow.readMotionCount(&deltaX, &deltaY, &SQUAL, &Shutter);  // read some of the data
+   opticalFlow.readBurstMode(dataArray); // use burst mode to read all of the data
+   }
 
-    iterations++;
-
-    if (gotMotionInterrupt){
-
-        gotMotionInterrupt = false;
-
-        sensor.readBurstMode(); // use burst mode to read all of the data
-    }
-
-    if (sensor.motionDataAvailable()) {
-
-        if (sensor.challengingSurfaceDetected()) {
-            Debugger::printf("Challenging surface detected!\n");
-        }
-
-        uint16_t deltaX = sensor.getDeltaX();
-        uint16_t deltaY = sensor.getDeltaY();
-        uint8_t shutterQuality = sensor.getSurfaceQuality();
-        uint8_t rawDataSum = sensor.getRawDataSum();
-        uint8_t rawDataMax = sensor.getRawDataMax();
-        uint8_t rawDataMin = sensor.getRawDataMin();
-        uint8_t lightMode = sensor.getLightMode();
-
-        // Report mode
-        switch (lightMode) {
-            case PAA3905::LIGHT_BRIGHT:
-                Debugger::printf("Bright Mode\n");
-                break;
-            case PAA3905::LIGHT_LOW:
-                Debugger::printf("Low Light Mode\n");
-                break;
-            case PAA3905::LIGHT_SUPER_LOW:
-                Debugger::printf("Super Low Light Mode\n");
-                break;
-            default:
-                Debugger::printf("Unknown Mode\n");
-        }
-
-        // Data and Diagnostics output
-
-        // Don't report data if under thresholds
-        if (sensor.goodQuality(lightMode, shutterQuality)) {
-            Debugger::printf("X: %d , Y: %d\n", deltaX, deltaY);
-        }
-
-        else {
-            Debugger::printf("Poor quality data; ignoring X,Y\n");
-        }
-
-        Debugger::printf("Number of Valid Features: %d\n", 4*shutterQuality);
-        Debugger::printf("Max raw Data: %d, Min raw Data: %d, Avg rawData: %d\n\n",
-                rawDataMax, rawDataMin, rawDataSum);
-    }
-
-    // Frame capture
-    if (iterations >= 100) { // capture one frame per 100 iterations (~5 sec) of navigation
+   if(dataArray[0] & 0x80) {   // Check if motion data available
     
-        iterations = 0;
-        Serial.println("Hold camera still for frame capture!");
-        delay(4000);
+     if(dataArray[0]  & 0x01) Serial.println("Challenging surface detected!");
 
-        uint32_t frameTime = millis();
-        sensor.enterFrameCaptureMode();   
-        sensor.captureFrame(frameArray);
-        sensor.exitFrameCaptureMode(); // exit fram capture mode
-        Serial.print("Frame time = "); Serial.print(millis() - frameTime); Serial.println(" ms"); Serial.println(" ");
+     deltaX = ((int16_t)dataArray[3] << 8) | dataArray[2];
+     deltaY = ((int16_t)dataArray[5] << 8) | dataArray[4];
+     SQUAL = dataArray[7];      // surface quality
+     RawDataSum = dataArray[8];
+     RawDataMax = dataArray[9];
+     RawDataMin = dataArray[10];
+     Shutter = ((uint32_t)dataArray[11] << 16) | ((uint32_t)dataArray[12] << 8) | dataArray[13];
+     Shutter &= 0x7FFFFF; // 23-bit positive integer 
 
-        for (uint8_t ii = 0; ii < 35; ii++) { // plot the frame data on the serial monitor (TFT display would be better)
-        
-            for (uint8_t jj = 0; jj < 35; jj++) {
-                uint8_t frameval = frameArray[ii*35 + jj];
-                Serial.print(frameval);
-                Serial.print(" ");  
-            }
-            Serial.println(" ");
+//   mode =    opticalFlow.getMode();
+     mode = (dataArray[1] & 0xC0) >> 6;  // mode is bits 6 and 7 
+     // Don't report data if under thresholds
+     if((mode == bright       ) && (SQUAL < 25) && (Shutter >= 0x00FF80)) deltaX = deltaY = 0;
+     if((mode == lowlight     ) && (SQUAL < 70) && (Shutter >= 0x00FF80)) deltaX = deltaY = 0;
+     if((mode == superlowlight) && (SQUAL < 85) && (Shutter >= 0x025998)) deltaX = deltaY = 0;
+   
+     // Report mode
+     if(mode == bright)        Serial.println("Bright Mode"); 
+     if(mode == lowlight)      Serial.println("Low Light Mode"); 
+     if(mode == superlowlight) Serial.println("Super Low Light Mode"); 
+     if(mode == unknown)       Serial.println("Unknown Mode"); 
+  
+     // Data and Diagnostics output
+     Serial.print("X: ");Serial.print(deltaX);Serial.print(", Y: ");Serial.println(deltaY);
+     Serial.print("Number of Valid Features: ");Serial.print(4*SQUAL);
+     Serial.print(", Shutter: 0x");Serial.println(Shutter, HEX);
+     Serial.print("Max Raw Data: ");Serial.print(RawDataMax);Serial.print(", Min Raw Data: ");Serial.print(RawDataMin);
+     Serial.print(", Avg Raw Data: ");Serial.println(RawDataSum); Serial.println(" ");
+   }
+
+  // Frame capture
+  if(iterations >= 100) // capture one frame per 100 iterations (~5 sec) of navigation
+  {
+    iterations = 0;
+    Serial.println("Hold camera still for frame capture!");
+    delay(4000);
+
+    frameTime = millis();
+    opticalFlow.enterFrameCaptureMode();   
+    opticalFlow.captureFrame(frameArray);
+    opticalFlow.exitFrameCaptureMode(); // exit fram capture mode
+    Serial.print("Frame time = "); Serial.print(millis() - frameTime); Serial.println(" ms"); Serial.println(" ");
+
+    for(uint8_t ii = 0; ii < 35; ii++) // plot the frame data on the serial monitor (TFT display would be better)
+      {
+        Serial.print(ii); Serial.print(" "); 
+        for(uint8_t jj = 0; jj < 35; jj++)
+        {
+        Serial.print(frameArray[ii*35 + jj]); Serial.print(" ");  
         }
         Serial.println(" ");
-
-        sensor.exitFrameCaptureMode(); // exit fram capture mode
-        Debugger::printf("Frame time = %d ms\n", millis() - frameTime);
-
-        // Return to navigation mode
-        sensor.reset(); // Reset PAA3905 to return all registers to default before configuring
-        delay(50);
-        sensor.setMode(DETECTION_MODE, AUTO_MODE);         // set modes
-        sensor.setResolution(RESOLUTION);         // set resolution fraction of default 0x2A
-        sensor.setOrientation(ORIENTATION);     // set orientation
-        sensor.status();          // clear interrupt before entering main loop
-        Debugger::printf("Back in Navigation mode!\n");
     }
+        Serial.println(" ");
 
-    delay(50); // limit reporting to 20 Hz
+    opticalFlow.exitFrameCaptureMode(); // exit fram capture mode
+    Serial.print("Frame time = "); Serial.print(millis() - frameTime); Serial.println(" ms"); Serial.println(" ");
+    
+    // Return to navigation mode
+    opticalFlow.reset(); // Reset PAA3905 to return all registers to default before configuring
+    delay(50);
+    opticalFlow.setMode(mode, autoMode);         // set modes
+    opticalFlow.setResolution(pixelRes);         // set resolution fraction of default 0x2A
+    opticalFlow.setOrientation(orient);          // set orientation
+    statusCheck = opticalFlow.status();          // clear interrupt before entering main loop
+    Serial.println("Back in Navigation mode!");
+  }
 
-    } // loop
+//  STM32L4.sleep();
+   delay(50); // limit reporting to 20 Hz
+    
+} // end of main loop
+
+
+void myIntHandler()
+{
+  motionDetect = true;
+}
--- src/PAA3905.h
+++ src/PAA3905.h
@@ -1,135 +1,95 @@
 /* PAA3905 Optical Flow Sensor
-
- * Copyright (c) 2021 Tlera Corporation and Simon D. Levy
+ * Copyright (c) 2021 Tlera Corporation
  *
- * MIT License
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
-#pragma once
+#ifndef __PAA3905_H
+#define __PAA3905_H
 
-#include <Arduino.h>
+#include "Arduino.h"
+#include <stdint.h>
+
+#define PAA3905_PRODUCT_ID            0x00 // default value 0xA2
+#define PAA3905_REVISION_ID           0x01
+#define PAA3905_MOTION                0x02
+#define PAA3905_DELTA_X_L             0x03
+#define PAA3905_DELTA_X_H             0x04
+#define PAA3905_DELTA_Y_L             0x05
+#define PAA3905_DELTA_Y_H             0x06
+#define PAA3905_SQUAL                 0x07
+#define PAA3905_RAWDATA_SUM           0x08
+#define PAA3905_MAX_RAWDATA           0x09
+#define PAA3905_MIN_RAWDATA           0x0A
+#define PAA3905_SHUTTER_L             0x0B
+#define PAA3905_SHUTTER_M             0x0C
+#define PAA3905_SHUTTER_H             0x0D
+#define PAA3905_RAWDATA_GRAB_STATUS   0x10
+#define PAA3905_RAWDATA_GRAB          0x13
+#define PAA3905_OBSERVATION           0x15
+#define PAA3905_MOTION_BURST          0x16
+#define PAA3905_POWER_UP_RESET        0x3A
+#define PAA3905_SHUTDOWN              0x3B
+#define PAA3905_RESOLUTION            0x4E
+#define PAA3905_ORIENTATION           0x5B
+#define PAA3905_INVERSE_PRODUCT_ID    0x5F // default value 0x5D
+
+#define standardDetectionMode      0
+#define enhancedDetectionMode      1
+
+#define autoMode012       0
+#define autoMode01        1
+
+#define bright        0
+#define lowlight      1
+#define superlowlight 2
+#define unknown       3
 
 class PAA3905 {
+public:
+  PAA3905(uint8_t cspin);
+  boolean begin(void);
+  uint8_t status();
+  void initRegisters(uint8_t mode);
+  void readMotionCount(int16_t *deltaX, int16_t *deltaY, uint8_t *SQUAL, uint32_t *Shutter);
+  void readBurstMode(uint8_t * dataArray); 
+  boolean checkID();
+  void setMode(uint8_t mode, uint8_t autoSwitch);
+  void setOrientation(uint8_t orient);
+  uint8_t getOrientation();
+  void setResolution(uint8_t res);
+  uint8_t getResolution();
+  void reset();
+  void shutdown();
+  void powerup();
+  uint8_t getMode();
+  void enterFrameCaptureMode();
+  uint8_t captureFrame(uint8_t * frameArray);
+  void exitFrameCaptureMode();
 
-    public:
-
-        typedef enum {
-            DETECTION_STANDARD,
-            DETECTION_ENHANCED
-        } detection_mode_t;
-
-        typedef enum {
-            AUTO_MODE_01,
-            AUTO_MODE_012,
-        } auto_mode_t;
-
-        typedef enum {
-            LIGHT_BRIGHT,
-            LIGHT_LOW,
-            LIGHT_SUPER_LOW,
-            LIGHT_UKNOWN
-        } light_mode_t;
-
-        typedef enum {
-            ORIENTATION_NORMAL  = 0x00,
-            ORIENTATION_XINVERT = 0x80,
-            ORIENTATION_YINVERT = 0x40,
-            ORIENTATION_SWAP    = 0x20,
-        } orientation_t;
-
-        PAA3905(uint8_t cspin);
-
-        void begin(void);
-
-        uint8_t status();
-
-        void initRegisters(uint8_t mode);
-
-        void readBurstMode(void); 
-
-        bool motionDataAvailable(void);
-
-        bool challengingSurfaceDetected(void);
-
-        uint16_t getDeltaX(void);
-
-        uint16_t getDeltaY(void);
-
-        uint8_t getSurfaceQuality(void);
-
-        uint8_t getRawDataSum(void);
-        
-        uint8_t getRawDataMax(void);
-
-        uint8_t getRawDataMin(void);
-
-        uint8_t getLightMode(void);
-
-        boolean checkID();
-
-        void setMode(uint8_t mode, uint8_t autoSwitch);
-
-        void setOrientation(uint8_t orient);
-
-        uint8_t getOrientation();
-
-        void setResolution(uint8_t res);
-
-        float getResolution();
-
-        void reset();
-
-        void shutdown();
-
-        void powerup();
-
-        uint8_t getMode();
-
-        void enterFrameCaptureMode();
-
-        void captureFrame(uint8_t * frameArray);
-
-        void exitFrameCaptureMode();
-
-        bool goodQuality(uint8_t lightMode, uint8_t shutterQuality);
-
-    private:
-
-        static const uint8_t  PAA3905_PRODUCT_ID            = 0x00; // default value = 0xA2
-        static const uint8_t  PAA3905_REVISION_ID           = 0x01;
-        static const uint8_t  PAA3905_MOTION                = 0x02;
-        static const uint8_t  PAA3905_DELTA_X_L             = 0x03;
-        static const uint8_t  PAA3905_DELTA_X_H             = 0x04;
-        static const uint8_t  PAA3905_DELTA_Y_L             = 0x05;
-        static const uint8_t  PAA3905_DELTA_Y_H             = 0x06;
-        static const uint8_t  PAA3905_SQUAL                 = 0x07;
-        static const uint8_t  PAA3905_RAWDATA_SUM           = 0x08;
-        static const uint8_t  PAA3905_MAX_RAWDATA           = 0x09;
-        static const uint8_t  PAA3905_MIN_RAWDATA           = 0x0A;
-        static const uint8_t  PAA3905_SHUTTER_L             = 0x0B;
-        static const uint8_t  PAA3905_SHUTTER_M             = 0x0C;
-        static const uint8_t  PAA3905_SHUTTER_H             = 0x0D;
-        static const uint8_t  PAA3905_RAWDATA_GRAB_STATUS   = 0x10;
-        static const uint8_t  PAA3905_RAWDATA_GRAB          = 0x13;
-        static const uint8_t  PAA3905_OBSERVATION           = 0x15;
-        static const uint8_t  PAA3905_MOTION_BURST          = 0x16;
-        static const uint8_t  PAA3905_POWER_UP_RESET        = 0x3A;
-        static const uint8_t  PAA3905_SHUTDOWN              = 0x3B;
-        static const uint8_t  PAA3905_RESOLUTION            = 0x4E;
-        static const uint8_t  PAA3905_ORIENTATION           = 0x5B;
-        static const uint8_t  PAA3905_INVERSE_PRODUCT_ID    = 0x5F; // default value = 0x5D
-
-        uint8_t _cs = 0;
-
-        uint8_t _data[14] = {};
-
-        void writeByte(uint8_t reg, uint8_t value);
-
-        void writeByteDelay(uint8_t reg, uint8_t value);
-
-        uint8_t readByte(uint8_t reg);
-
-        void standardDetection(void);
-
-        void enhancedDetection(void);
+private:
+  uint8_t _cs, _mode;
+  void writeByte(uint8_t reg, uint8_t value);
+  void writeByteDelay(uint8_t reg, uint8_t value);
+  uint8_t readByte(uint8_t reg);
+  void standardDetection(void);
+  void enhancedDetection(void);
 };
+
+#endif //__PAA3905_H
