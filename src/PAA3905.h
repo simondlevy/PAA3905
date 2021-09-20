d8c238d211b4b856bcb7724338fd6a882bb4f4d7
simondlevy <simon.d.levy@gmail.com>
2021-09-20 19:37:01 -0400

    =More restoration

--- src/PAA3905.h
+++ src/PAA3905.h
@@ -1,30 +1,13 @@
 /* PAA3905 Optical Flow Sensor
- * Copyright (c) 2021 Tlera Corporation
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * Copyright (c) 2021 Tlera Corporation and Simon D. Levy
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * MIT License
  */
 
-#ifndef __PAA3905_H
-#define __PAA3905_H
+#pragma once
 
 #include "Arduino.h"
-#include <stdint.h>
 
 #define PAA3905_PRODUCT_ID            0x00 // default value 0xA2
 #define PAA3905_REVISION_ID           0x01
@@ -91,5 +74,3 @@ private:
   void standardDetection(void);
   void enhancedDetection(void);
 };
-
-#endif //__PAA3905_H
