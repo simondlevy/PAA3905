bff17cb884d4bbfc0edba987207bb4e4719fe926
simondlevy <simon.d.levy@gmail.com>
2022-12-12 00:44:34 -0500

    Moved frame-capture code out of motion-capture sketch

--- README.md
+++ README.md
@@ -5,12 +5,38 @@
 Header-only Arduino library for the
 [PAA3905 optical flow camera](https://www.tindie.com/products/onehorse/paa3905-optical-flow-camera/)
 
-This code is based on Kris Winer's PAA3905 [sketches](https://github.com/kriswiner/PAA3905).  As usual, I took
-Kris's code an made an Arduino library that you can clone into your Arduino/libraries folder to
-run examples and use for other projects.
+The sensor uses standard SPI for communications at a maximum serial port
+speed of 2 MHz. The sensor data ready is signaled by an active LOW
+interrupt.
+
+This sensor offers two sensitivities: standard detection and enhanced
+detection for rough terrain at > 15 cm height. The sensor can automatically
+switch between bright (>60 lux), low light (>30 lux), and super low light (> 5
+lux) conditions. Bright and low light modes work at 126 frames per second. The
+super low light mode is limited to 50 frames per second.
+
+The sensor uses typically 3.5 mA in operation and has a 12 uA shutdown mode.
+
+The sensor can operate in navigate mode producing delta X and Y values which
+are proportional to lateral velocity.  The limiting speed is determined by the
+maximum 7.2 rads/sec flow rate and by distance to the measurement surface; 80
+mm is the minimum measurement distance. So at 80 mm the maxium speed is 0.576
+m/s (1.25 mph), at 2 meter distance (~drone height) the maximum speed is 14.4
+m/s (32 mph), etc.
+
+The sensor can also operate in raw data (frame grab) mode producing 35 x 35
+pixel images from the sensor at a frame rate of ~15 Hz. This makes the PAA3905
+an inexpensive, low-resolution, infrared-sensitive video camera.
+
+This code is based on Kris Winer's PAA3905
+[sketches](https://github.com/kriswiner/PAA3905).  As usual, I took Kris's code
+an made an Arduino library that you can clone into your Arduino/libraries
+folder to run examples and use for other projects.
 
 I have tested this library on the following platforms:
 
 * Teensy 4.0 from PJRC
 
 * Ladybug STM32L4 from Tlera Corporation
+
+* TinyPICO ESP32
--- examples/MotionCapture/MotionCapture.ino
+++ examples/MotionCapture/MotionCapture.ino
@@ -1,29 +1,5 @@
 /*
-   Demo of the PAA3905 optical flow sensor
-
-   This sketch configures and reads data from the PAA3905 optical flow sensor.
-   The sensor uses standard SPI for communications at a maximum serial port
-   speed of 2 MHz. The sensor data ready is signaled by an active LOW
-   interrupt.
-
-   This sensor offers two sensitivities: standard detection and enhanced
-   detection for rough terrain at > 15 cm height. The sensor can automatically
-   switch between bright (>60 lux), low light (>30 lux), and super low light (> 5
-   lux) conditions. Bright and low light modes work at 126 frames per second. The
-   super low light mode is limited to 50 frames per second.
-
-   The sensor uses typically 3.5 mA in operation and has a 12 uA shutdown mode.
-
-   The sensor can operate in navigate mode producing delta X and Y values which
-   are proportional to lateral velocity.  The limiting speed is determined by the
-   maximum 7.2 rads/sec flow rate and by distance to the measurement surface; 80
-   mm is the minimum measurement distance. So at 80 mm the maxium speed is 0.576
-   m/s (1.25 mph), at 2 meter distance (~drone height) the maximum speed is 14.4
-   m/s (32 mph), etc.
-
-   The sensor can also operate in raw data (frame grab) mode producing 35 x 35
-   pixel images from the sensor at a frame rate of ~15 Hz. This makes the PAA3905
-   an inexpensive, low-resolution, infrared-sensitive video camera.
+   the PAA3905 optical flow sensor motion detection example
 
    Copyright (c) 2021 Tlera Corporiation and Simon D. Levy
 
@@ -77,10 +53,6 @@ void setup()
 
 void loop()
 {
-    static uint8_t iterations;
-
-    iterations++;
-
     // Navigation
     if (gotMotionInterrupt) {
 
@@ -124,49 +96,6 @@ void loop()
                 rawDataMax, rawDataMin, rawDataSum);
     }
 
-    // Frame capture
-    if (iterations >= 100) // capture one frame per 100 iterations (~5 sec) of navigation
-    {
-        iterations = 0;
-        Debugger::printf("\nHold camera still for frame capture!\n");
-        delay(4000);
-
-        uint32_t frameTime = millis();
-        static uint8_t frameArray[1225];
-        _sensor.enterFrameCaptureMode();   
-        _sensor.captureFrame(frameArray);
-        _sensor.exitFrameCaptureMode(); // exit fram capture mode
-        Debugger::printf("Frame time = %d ms\n", millis() - frameTime);
-
-        for (uint8_t ii = 0; ii < 35; ii++) {
-            Serial.print(ii); Serial.print(" "); 
-            for (uint8_t jj = 0; jj < 35; jj++)
-            {
-                Serial.print(frameArray[ii*35 + jj]); Serial.print(" ");  
-            }
-            Serial.println(" ");
-        }
-        Serial.println(" ");
-
-        _sensor.exitFrameCaptureMode(); // exit fram capture mode
-        Debugger::printf("Frame time = %d ms\n", millis() - frameTime);
-
-        static const PAA3905::detectionMode_t DETECTION_MODE = PAA3905::DETECTION_STANDARD;
-        static const PAA3905::autoMode_t AUTO_MODE           = PAA3905::AUTO_MODE_01;
-        static const PAA3905::orientation_t ORIENTATION       = PAA3905::ORIENTATION_NORMAL;
-        static const uint8_t RESOLUTION                       = 0x2A; // 0x00 to 0xFF
-
-        // Return to navigation mode
-        _sensor.reset(); // Reset PAA3905 to return all registers to default before configuring
-        delay(50);
-        _sensor.setMode(DETECTION_MODE, AUTO_MODE); // set modes
-        _sensor.setResolution(RESOLUTION);         // set resolution fraction of default 0x2A
-        _sensor.setOrientation(ORIENTATION);
-        _sensor.clearInterrupt();
-        Debugger::printf("Back in Navigation mode!\n");
-    }
-
-    //  STM32L4.sleep();
     delay(50); // limit reporting to 20 Hz
 
 } // loop
