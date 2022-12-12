47ad13657c850982d8a9adca1aa526f8baeec77f
simondlevy <simon.d.levy@gmail.com>
2022-12-12 01:43:09 -0500

    Removed navigation-mode code from frame-capture sketch

--- examples/FrameCapture/FrameCapture.ino
+++ examples/FrameCapture/FrameCapture.ino
@@ -47,13 +47,14 @@ void loop()
 
         _sensor.enterFrameCaptureMode();   
         _sensor.captureFrame(frameArray);
-     
         _sensor.exitFrameCaptureMode(); // exit fram capture mode
 
         Debugger::printf("Frame time = %d ms\n", millis() - _lastCaptureMsec);
 
         for (uint8_t j = 0; j < 35; j++) {
+
             Debugger::printf("%2d ", j);
+
             for (uint8_t k = 0; k < 35; k++) {
 
                 // Debugger::printf() would be too slow here
@@ -67,22 +68,6 @@ void loop()
         _sensor.exitFrameCaptureMode(); 
 
         Debugger::printf("Frame time = %d ms\n", millis() - _lastCaptureMsec);
-
-        static const PAA3905::detectionMode_t DETECTION_MODE = PAA3905::DETECTION_STANDARD;
-        static const PAA3905::autoMode_t AUTO_MODE           = PAA3905::AUTO_MODE_01;
-        static const PAA3905::orientation_t ORIENTATION       = PAA3905::ORIENTATION_NORMAL;
-        static const uint8_t RESOLUTION                       = 0x2A; // 0x00 to 0xFF
-
-        // Return to navigation mode
-        _sensor.reset(); // Reset PAA3905 to return all registers to default before configuring
-
-        delay(50);
-
-        _sensor.setMode(DETECTION_MODE, AUTO_MODE); // set modes
-        _sensor.setResolution(RESOLUTION);         // set resolution fraction of default 0x2A
-        _sensor.setOrientation(ORIENTATION);
-        _sensor.clearInterrupt();
-        Debugger::printf("Back in Navigation mode!\n");
     }
 
 } // loop
